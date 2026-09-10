e5ae505791e646ebdfb622f9829934fcc26970dc
Milton <mapenacasa@gmail.com>
2026-09-10 11:43:00 -0500

    feat: skeleton con cantidad de rayitas configurable

--- src/components/SkeletonLista.jsx
+++ src/components/SkeletonLista.jsx
@@ -1,5 +1,9 @@
-export function SkeletonLista() {
+export function SkeletonLista({ lineas = 3 }) {
     return (
-        <div className="skeleton-lista"></div>
+        <div className="skeleton-lista">
+            {Array.from({ length: lineas}).map((_, i) => (
+                <div key={i} className="skeleton-item"></div>
+            ))}
+        </div>
     );
 }
